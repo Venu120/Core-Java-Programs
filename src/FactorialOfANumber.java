f0768c999e74ab12da397369ca7b33c4c5bce4cb
Anirban Dey <anirbandey303@gmail.com>
2020-08-10 15:13:21 +0530

    Factorial - recursive solution

--- src/FactorialOfANumber.java
+++ src/FactorialOfANumber.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class FactorialOfANumber {
   public static int factorialLoop(int n){
     int factorial = 1;
@@ -6,8 +7,20 @@ public class FactorialOfANumber {
     }
     return factorial;
   }
+  public static int factorialRecursive(int n){
+    if(n>1){
+      return n*factorialRecursive(n-1);
+    }
+    else{
+      return 1;
+    }
+  }
   public static void main(String[] args){
-    int n = 5;
-    System.out.println("Factorial of " + n + " is = "+ factorialLoop(n));
+    //int n = 5;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number = ");
+    int n = sc.nextInt();
+    System.out.println("(Loop) Factorial of " + n + " is = "+ factorialLoop(n));
+    System.out.println("(Recursive) Factorial of " + n + " is = "+ factorialRecursive(n));
   }
 }
