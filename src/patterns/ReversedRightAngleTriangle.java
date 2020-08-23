4dd12b8d1bd6d8b43ff10f38c4d6e5d1281a93cb
Anirban Dey <anirbandey303@gmail.com>
2020-08-23 22:11:03 +0530

    Right-Angle Triangle Pattern

---
+++ src/patterns/ReversedRightAngleTriangle.java
@@ -0,0 +1,20 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class ReversedRightAngleTriangle {
+  public static void printPattern(int n) {
+    for(int i = 1; i <= n; i++) {
+      for(int j = n; j >= i; j-- ) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of rows = ");
+    int n = sc.nextInt();
+    printPattern(n);
+  }
+}
